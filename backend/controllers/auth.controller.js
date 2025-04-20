9c5c99715495a726d7597fa72e5ba5e5ed6fca76
FIDELIS TUWEI <fideliofidel9@gmail.com>
2025-04-20 10:44:58 +0300

    admin login route logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,4 +9,38 @@ const client = require('../utils/redis');
 // admin login route
 exports.adminRoute = asyncHandler ( async (req, res) => {
     const { email, password } = req.body;
-})
+
+    try {
+        const [result] = await query('SELECT * FROM Users WHERE email = ?', [email]);
+        const admin = result[0];
+
+        if (!admin) {
+            return res.status(401).json({ status: 'error', message: "Invalid email or password" });
+        };
+
+        const passwordMatch = await bcrypt.compare(password, admin.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ status: 'error', message: "Invalid email or password" })
+        };
+
+        const { accessToken, refreshToken } = generateTokens(admin.user_id);
+        await storeRefreshToken(admin.user_id, refreshToken);
+
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(200).json({
+            status: "success",
+            message: "Admin Login successful",
+            data: {
+                id: admin.user_id,
+                username: admin.username,
+                email: admin.email,
+                role: admin.role
+            }
+        });
+        
+    } catch (error) {
+        console.error("Error Occured on adminRoute controller", error);
+        return res.status(500).json({ status: "error", message: "Internal Server Error" });
+    }
+});
