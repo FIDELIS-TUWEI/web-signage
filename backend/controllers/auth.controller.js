36d1e36fe03169ca6d2fff00222bdec38ee07d49
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2025-04-21 06:26:56 +0300

    admin route logic controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const { generateTokens, storeRefreshToken, setCookies } = require('../utils/gene
 const config = require('../utils/config');
 const client = require('../utils/redis');
 
-// admin login route
+// admin login
 exports.adminRoute = asyncHandler ( async (req, res) => {
     const { email, password } = req.body;
 
@@ -42,5 +42,71 @@ exports.adminRoute = asyncHandler ( async (req, res) => {
     } catch (error) {
         console.error("Error Occured on adminRoute controller", error);
         return res.status(500).json({ status: "error", message: "Internal Server Error" });
+    }
+});
+
+// get admin profile
+exports.getAdminProfile = asyncHandler (async (req, res) => {
+    try {
+        res.status(200).json({
+            status: "success",
+            message: "Admin profile fetched successfully",
+            data: req.user
+        })
+    } catch (error) {
+        console.error("Error occured on getAdminProfile controller", error);
+        return res.status(500).json({ status: "error", message: "Internal Server Error" })
+    }
+});
+
+// logout
+exports.logout = asyncHandler (async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (refreshToken) {
+            const decoded = jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
+            await client.del(`refresh_token: ${decoded.userId}`);
+        };
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+
+        res.status(200).json({ status: "success", message: "Logged out successfully" });
+    } catch (error) {
+        console.error("Error occured on logout controller", error);
+        return res.status(500).json({ status: "error", message: "Internal server error" })
+    }
+});
+
+// refreshtoken
+exports.refreshToken = asyncHandler (async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) {
+            return res.status(401).json({ status: "error", message: "No refresh Token provided" })
+        };
+
+        const decoded = jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
+        const storedToken = await client.get(`refresh_token: ${decoded.userId}`);
+
+        if (storedToken !== refreshToken) {
+            return res.status(401).json({ status: "error", message: "Invalid refresh Token" });
+        };
+
+        const accessToken = jwt.sign({ userId: decoded.userId }, config.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: config.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 60 * 1000
+        });
+
+        res.status(200).json({ status: "success", message: "Token refreshed successfully" });
+
+    } catch (error) {
+        console.error("Error Occurred on adminLogout controller:", error);
+        return res.status(500).json({ status: "error", message: "Internal server error" });
     }
 });
