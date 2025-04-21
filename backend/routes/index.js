4d6922e5dbe9d2ca4504472be708d7917b2ac468
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2025-04-21 06:37:21 +0300

    route index file logic

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const authRoutes = require("./auth.routes");
+
+router.use("/auth", authRoutes);
+
 router.get("/", (req, res) => {
     res.status(200).json({ status: "success", message: `Welcome to the Backend API Server` });
 });
